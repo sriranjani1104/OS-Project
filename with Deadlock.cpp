5a54e11a05d5857060f7835c6fed30d33e76dd0a
sriranjani1104 <154339587+sriranjani1104@users.noreply.github.com>
2025-11-19 22:03:11 +0530

    with Deadlock.cpp

---
+++ with Deadlock.cpp
@@ -0,0 +1,25 @@
+void runWithDeadlock() {
+    RAG g;
+
+    // Nodes
+    g.addNode("P1"); g.addNode("P2"); g.addNode("P3"); g.addNode("P4");
+    g.addNode("R1"); g.addNode("R2"); g.addNode("R3"); g.addNode("R4");
+
+    // Edges WITH Deadlock (cycle exists)
+    g.addEdge("P1", "R1");
+    g.addEdge("R1", "P2");
+    g.addEdge("P2", "R2");
+    g.addEdge("R2", "P3");
+    g.addEdge("P3", "R3");
+    g.addEdge("R3", "P4");
+    g.addEdge("P4", "R4");
+    g.addEdge("R4", "P1"); // <-- Creates deadlock cycle
+
+    g.printGraph();
+
+    cout << "\nStatus: ";
+    if (g.detectCycle())
+        cout << "Deadlock Detected\n";
+    else
+        cout << "No Deadlock\n";
+}
